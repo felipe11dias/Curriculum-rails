d1f690af27384ef7ea14132afecadf2170c1d7c7
Felipe Dias <clame1997mac@hotmail.com>
2019-05-13 17:10:01 -0300

    Finalizando página HomePage

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module CurriculumRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
